82347ae519a2fe35378b3fd868352f960bf2e8f9
Cesar Molina <cesaruxgxtx@gmail.com>
2021-11-19 15:21:29 -0500

    fixing 7-division.c

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -2,15 +2,27 @@
 #include <math.h>
 
 /**
- * division - perform the division operation to complex numbers
- * @c1: first complex number
- * @c2: second complex number
- * @c3: quotient of complex numbers
- * Return: void
+ * division - performs the division operation to complex numbers.
+ * @c1: Struct type complex.
+ * @c2: Struct type complex.
+ * @c3: Pointer to complex result.
+ * Return: N/A.
  */
-
 void division(complex c1, complex c2, complex *c3)
 {
-	c3->re = ((c1.re * c2.re) + (c1.im * c2.im)) / (pow(c2.re, 2) + pow(c2.im, 2));
-	c3->im = ((c1.im * c2.re) - (c1.re * c2.im)) / (pow(c2.re, 2) + pow(c2.im, 2));
+	double x, y, z;
+
+	if (c2.re == 0 && c2.im == 0)
+	{
+		printf("Division b 0 + 0i isn't allowed.");
+		return;
+	}
+	else
+	{
+		x = c1.re * c2.re + c1.im * c2.im;
+		y = c1.im * c2.re - c1.re * c2.im;
+		z = c2.re * c2.re + c2.im * c2.im;
+		(*c3).re = x / z;
+		(*c3).im = y / z;
+	}
 }
